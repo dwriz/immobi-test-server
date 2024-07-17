83ae34a27e2034fabfb62578fb729c16982ac89f
dwriz <dwikyrizkya27@gmail.com>
2024-07-17 23:39:27 +0700

    add migration and model config

---
+++ models/department.js
@@ -0,0 +1,19 @@
+"use strict";
+const { Model } = require("sequelize");
+module.exports = (sequelize, DataTypes) => {
+  class Department extends Model {
+    static associate(models) {
+      Department.hasMany(models.Jabatan, { foreignKey: "id_department" });
+    }
+  }
+  Department.init(
+    {
+      nama_department: DataTypes.STRING,
+    },
+    {
+      sequelize,
+      modelName: "Department",
+    }
+  );
+  return Department;
+};
---
+++ models/karyawan.js
@@ -0,0 +1,24 @@
+"use strict";
+const { Model } = require("sequelize");
+module.exports = (sequelize, DataTypes) => {
+  class Karyawan extends Model {
+    static associate(models) {
+      Karyawan.belongsTo(models.Jabatan, { foreignKey: "id_jabatan" });
+    }
+  }
+  Karyawan.init(
+    {
+      name: DataTypes.STRING,
+      id_jabatan: DataTypes.INTEGER,
+      age: DataTypes.INTEGER,
+      gender: DataTypes.STRING,
+      tanggal_lahir: DataTypes.DATE,
+      alamat: DataTypes.STRING,
+    },
+    {
+      sequelize,
+      modelName: "Karyawan",
+    }
+  );
+  return Karyawan;
+};
