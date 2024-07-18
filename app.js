81d6569d4704f2395f1e8b9f7e7efb11d81af03a
dwriz <dwikyrizkya27@gmail.com>
2024-07-18 09:24:37 +0700

    add express json config

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const port = 3000;
 const cors = require("cors");
 app.use(cors());
 
+app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 const router = require("./routes");
